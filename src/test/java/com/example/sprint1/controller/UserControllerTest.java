e2a6511004e30f344c5e060588855107d68233e1
mtgarcia93 <txai.garcia@mercadolibre.com.mx>
2024-05-08 21:36:27 -0600

    Adding individual integration testing

--- src/test/java/com/example/sprint1/controller/UserControllerTest.java
+++ src/test/java/com/example/sprint1/controller/UserControllerTest.java
@@ -1,13 +1,16 @@
 package com.example.sprint1.controller;
 
 import com.example.sprint1.dto.CountFollowersUserDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -18,13 +21,28 @@ public class UserControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+
+    /**
+     * Test case to verify the successful retrieval of followers count for a user.
+     *
+     * @throws Exception if there is an error during the test execution
+     */
     @Test
     void getFollowersCountHappyPath() throws Exception {
-        CountFollowersUserDto = new CountFollowersUserDto();
+        CountFollowersUserDto dto = CountFollowersUserDto.builder()
+                .userId(2)
+                .userName("user2")
+                .count(0)
+                .build();
+        String expectedJson = new ObjectMapper().writeValueAsString(dto); // Convert dto to json
+
         String id = "2";
         String path = "/users/" + id + "/followers/count";
+
         this.mockMvc.perform(get(path))
                 .andDo(print())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(expectedJson)) // Verify the expected json
                 .andExpect(status().isOk());
     }
 }
